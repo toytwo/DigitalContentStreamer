99f43fda63cfc85abbd7efa18de9223dfe235a1f
Jackson <alexman.jackson@gmail.com>
2026-05-01 11:44:15 -0500

    NavBar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,6 +28,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">{children}</body>
-    </html>
+   </html>
   );
 }
