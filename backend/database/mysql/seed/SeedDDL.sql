a1329f69bb704c75f3871c51b28e5c9f1274d35f
Jackson <alexman.jackson@gmail.com>
2026-05-01 20:00:46 -0500

    Made phone number unique

--- backend/database/mysql/seed/SeedDDL.sql
+++ backend/database/mysql/seed/SeedDDL.sql
@@ -11,7 +11,7 @@ CREATE TABLE Region (
 CREATE TABLE User (
     user_id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    phone_number INT NOT NULL,
+    phone_number INT NOT NULL UNIQUE,
     user_role VARCHAR(20) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
